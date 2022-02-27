66e0dd54baf5e2ded7151884b86b714eea9a5d7f
shalondelp <del-padre@hotmail.com>
2022-02-27 17:22:20 -0400

    calculadora.java

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,9 +9,6 @@ public class PrimeiroPrograma {
         System.out.println(gato);
 
 
-        Livro livro = new Livro("O Problemas dos 3 corpos",300);
-        System.out.println(livro);
-
         /*int a = 2;
         int b = 3;
         System.out.println("Hello World! " + (a+b));*/
@@ -20,39 +17,3 @@ public class PrimeiroPrograma {
 }
 
 
-class Livro {
-
-    private string nome;
-    private integer numPaginas;
-
-
-    public Livro(string nome, integer numPaginas) {
-        this.nome = nome;
-        this.numPaginas = numPaginas;
-    }
-
-
-    public string getNome() {
-        return nome;
-    }
-
-    public void setNome(string nome) {
-        this.nome = nome;
-    }
-
-    public integer getNumPaginas() {
-        return numPaginas;
-    }
-
-    public void setNumPaginas(integer numPaginas) {
-        this.numPaginas = numPaginas;
-    }
-
-    @Override
-    public String toString() {
-        return "Livro{" +
-                "nome=" + nome +
-                ", numPaginas=" + numPaginas +
-                '}';
-    }
-}
